340816915feab7c3a73de0c3514f9988f1bed159
Jonathan Morrell <jmorrell@berkeley.edu>
2018-12-07 10:02:51 -0800

    First Commit

---
+++ test/testthat/test_ars.R
@@ -0,0 +1,11 @@
+###########################################################
+## STAT 243 Final Project
+## Adaptive Rejection Sampling Algorithm
+##
+## Date: 12/12/18
+## Authors: Jonathan Morrell, Ziyang Zhou, Vincent Zijlmans
+###########################################################
+
+test_that("Dummy Test", {
+  expect_equal(str_length("ab"), 1)
+})
